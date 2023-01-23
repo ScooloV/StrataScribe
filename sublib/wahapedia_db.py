5ce85f8058e4ff27b83a33f50cd8c545f237b580
ScooloV <scoolov@mail.ru>
2023-01-23 13:55:07 +0100

    Update wahapedia_db.py

--- sublib/wahapedia_db.py
+++ sublib/wahapedia_db.py
@@ -6,13 +6,26 @@ from datetime import datetime, timedelta
 
 import requests
 
+# URL for the Wahapedia website
 wahapedia_url = "https://wahapedia.ru/wh40k9ed/"
+
+# List of all CSV files to be downloaded from the website
 wahapedia_csv_list = ["Datasheets.csv", "Datasheets_stratagems.csv", "Factions.csv", "StratagemPhases.csv", "Stratagems.csv", "Last_update.csv"]
+
+# Path to the local directory where the CSV files will be saved
 wahapedia_path = os.path.abspath("./wahapedia")
+
+# Path to the file that keeps track of the last time the CSV files were updated
 file_list_path = os.path.abspath(os.path.join(wahapedia_path, "_file_list.json"))
 
 
 def init_db():
+    """
+    Initializes the local database by checking if the CSV files need to be updated.
+    If the local directory or the file list do not exist, they will be created.
+    If a CSV file does not exist or is older than 1 day, it will be downloaded from the website.
+    :return: True if any of the CSV files were updated, False otherwise
+    """
     check_csv_update = False
     if not os.path.exists(wahapedia_path):
         os.mkdir(wahapedia_path)
@@ -28,6 +41,11 @@ def init_db():
 
 
 def _wahapedia_has_update():
+    """
+    Checks if the Wahapedia website has any new updates by comparing the last_update field from the Last_update.csv file.
+    If the file does not exist or the last_update field has changed, it will download the file.
+    :return: True if the website has new updates, False otherwise
+    """
     last_update_path = os.path.join(wahapedia_path, "Last_update.csv")
 
     if not os.path.exists(last_update_path):
@@ -45,6 +63,13 @@ def _wahapedia_has_update():
 
 
 def _check_csv(csv_name):
+    """
+    Checks if the specified CSV file needs to be updated by comparing its last update time with the file list.
+    If the file does not exist or is older than 1 day, it will be downloaded from the website and the file list will be updated.
+:param csv_name: the name of the CSV file to be checked
+:return: True if the file was updated, False otherwise
+"""
+
     csv_updated = False
     if not (os.path.exists(os.path.join(wahapedia_path, csv_name)) and _check_file_list(csv_name)):
         print(csv_name + " is bad, redownloading!")
@@ -57,6 +82,12 @@ def _check_csv(csv_name):
 
 
 def _check_file_list(csv_name):
+    """
+    Checks if the specified CSV file is in the file list and if it was updated in the last 1 day.
+    If the file list does not exist, it will be created.
+    :param csv_name: the name of the CSV file to be checked
+    :return: True if the file is in the file list and was updated in the last 1 day, False otherwise
+    """
     if not os.path.exists(file_list_path):
         print(file_list_path + " not exists, creating new")
         _create_file_list()
@@ -73,6 +104,9 @@ def _check_file_list(csv_name):
 
 
 def _create_file_list():
+    """
+    Creates the file list with the current time for all the CSV files in the wahapedia_csv_list.
+    """
     empty_file_list = {}
     with open(file_list_path, "w") as fl:
         for wahapedia_csv in wahapedia_csv_list:
@@ -81,6 +115,10 @@ def _create_file_list():
 
 
 def _register_file_list(csv_path):
+    """
+    Registers the specified CSV file in the file list with the current time.
+    :param csv_path: the path of the CSV file to be registered
+    """
     with open(file_list_path, "r") as fl:
         current_file_list_json = json.load(fl)
 
@@ -91,18 +129,27 @@ def _register_file_list(csv_path):
 
 
 def _download_file(file_url, folder_name):
+    """
+    Downloads a file from a specified url and saves it to a specified folder.
+    :param file_url: the url of the file to be downloaded
+    :param folder_name: the folder where the file will be saved
+    :return: the path where the file was saved
+    """
     file_name = file_url.split("/")[-1]
     save_path = os.path.abspath(os.path.join(folder_name, file_name))
 
     file_downloaded = False
 
+    # Keep trying to download the file until it is successfully downloaded
     while not file_downloaded:
         get_response = requests.get(file_url, stream=True)
+        # Open the file and write the content in chunks
         with open(save_path, 'wb') as f:
             for chunk in get_response.iter_content(chunk_size=1024):
                 if chunk:  # filter out keep-alive new chunks
                     f.write(chunk)
         file_size = os.stat(save_path).st_size
+        # Check if the file size is smaller than 2048 bytes, which could indicate that anti-spam is working
         if file_size < 2048 and file_name != "Last_update.csv":
             print("Anti Spam is working. Waiting for 5 second to try again.")
             time.sleep(5)
@@ -112,9 +159,15 @@ def _download_file(file_url, folder_name):
     return save_path
 
 
-def get_dict_from_csv(csv_file_name):
+
+def get_dict_from_csv(csv_path):
+    """
+    Converts a CSV file to a list of dictionaries
+    :param csv_path: the path of the CSV file to be converted
+    :return: a list of dictionaries
+    """
     results = []
-    csv_file_path = os.path.abspath(os.path.join(wahapedia_path, csv_file_name))
+    csv_file_path = os.path.abspath(os.path.join(wahapedia_path, csv_path))
     with open(csv_file_path, 'r', encoding='ascii', errors='ignore') as csvfile:
         reader = csv.DictReader(csvfile, delimiter='|', quoting=csv.QUOTE_NONE)
         for row in reader:
